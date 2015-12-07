16a3d109213f7979ebf9bd025ad2630fe17b9627
Paaadi <Paaadi@dhcp-10-181-31-70.dynamic.eduroam.mwn.de>
2015-12-07 13:29:19 +0100

    Wechsel zwischen verschiedenen Ansichten
    Cardboard3D, CardboardVR und Overview

--- Assets/Scripts/PluginScript.cs
+++ Assets/Scripts/PluginScript.cs
@@ -4,18 +4,29 @@ using System.Collections;
 
 public class PluginScript : MonoBehaviour {
 
+	enum State {Overview, CardboardVR, Cardboard3D};
+
 	public Text textLeft;
 	public Text textRight;
-	public Transform cardboard;
+	
+	public GameObject ceiling;
+
+	public Camera overviewCam;
+
+	public Cardboard cardboard;
 	public Transform head;
+	public GameObject cardboardCam;
+	public GameObject sphere;
 
 	private AndroidJavaObject bridge;
+
+	private State state;
 
 	// Use this for initialization
 	void Start () {
 		bridge = new AndroidJavaObject ("com.example.player.Bridge");
 
-
+		state = State.CardboardVR;
 	}
 	
 	// Update is called once per frame
@@ -26,18 +37,49 @@ public class PluginScript : MonoBehaviour {
 				textLeft.text = "Double Klick";
 				bridge.Call ("findPosition");
 			}
-			else if (touches [0].phase.Equals (TouchPhase.Stationary)) {
+			else if (touches[0].phase.Equals (TouchPhase.Stationary)) {
 				float radian = ((2.0f * Mathf.PI) / 360.0f) * head.rotation.eulerAngles.y;
 				float xFactor = Mathf.Sin (radian);
 				float yFactor = Mathf.Cos (radian);
-				cardboard.position = new Vector3 (cardboard.position.x + (xFactor * 0.1f), cardboard.position.y,
-				                                 cardboard.position.z + (yFactor * 0.1f));
+				cardboard.transform.position = new Vector3 (cardboard.transform.position.x + (xFactor * 0.1f), cardboard.transform.position.y,
+				                                 cardboard.transform.position.z + (yFactor * 0.1f));
+			}
+			else if (state != State.Overview
+			         && touches[0].phase.Equals (TouchPhase.Moved)
+			         && touches[0].deltaPosition.y > 10.0f) {
+				state = State.Overview;
+				cardboardCam.SetActive(false);
+				sphere.SetActive(true);
+				overviewCam.gameObject.SetActive(true);
+				ceiling.SetActive(false);
+			}
+			else if (state == State.Overview
+			         && touches[0].phase.Equals (TouchPhase.Moved)
+			         && touches[0].deltaPosition.y < -10.0f) {
+				state = State.CardboardVR;
+				ceiling.SetActive(true);
+				overviewCam.gameObject.SetActive(false);
+				sphere.SetActive(false);
+				cardboardCam.SetActive(true);
+				cardboard.VRModeEnabled = true;
+			}
+			else if (state == State.CardboardVR
+			         && touches[0].phase.Equals (TouchPhase.Moved)
+			         && touches[0].deltaPosition.x > 10.0f) {
+				state = State.Cardboard3D;
+				cardboard.VRModeEnabled = false;
+			}
+			else if (state == State.Cardboard3D
+			         && touches[0].phase.Equals (TouchPhase.Moved)
+			         && touches[0].deltaPosition.x < -10.0f) {
+				state = State.CardboardVR;
+				cardboard.VRModeEnabled = true;
 			}
 		}
 		else if (touches.Length == 2) {
-			if (touches [0].phase.Equals (TouchPhase.Began)
-			    && touches [1].phase.Equals (TouchPhase.Began)) {
-				bridge.Call ("recordFingerprint", cardboard.position.x, cardboard.position.z);
+			if (touches[0].phase.Equals (TouchPhase.Began)
+			    && touches[1].phase.Equals (TouchPhase.Began)) {
+				bridge.Call ("recordFingerprint", cardboard.transform.position.x, cardboard.transform.position.z);
 			}
 		}
 	}
@@ -55,7 +97,7 @@ public class PluginScript : MonoBehaviour {
 		if (parameterArray.Length == 3) {
 			float xPosition = float.Parse(parameterArray[0]);
 			float zPosition = float.Parse(parameterArray[1]);
-			cardboard.position = new Vector3(xPosition, cardboard.position.y, zPosition);
+			cardboard.transform.position = new Vector3(xPosition, cardboard.transform.position.y, zPosition);
 
 			textLeft.text = "X: " + xPosition.ToString() + ", Z: " + zPosition.ToString();
 			textRight.text = parameterArray[2];
