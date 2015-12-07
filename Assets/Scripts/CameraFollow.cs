a2e397edab88a839d5a2f030f332e420be34406b
Paaadi <Paaadi@Paaadi.local>
2015-12-07 19:05:43 +0100

    Overview Camera folgt der Position

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+	
+	public Transform movementTarget;
+	public Transform rotationTarget;
+	
+	Vector3 offset;
+	
+	void Start() {
+		transform.position = new Vector3(movementTarget.position.x, 10.0f, movementTarget.position.z);
+		offset = transform.position - movementTarget.position;
+		transform.position = movementTarget.position + offset;
+		
+		Quaternion q = new Quaternion();
+		q.eulerAngles = new Vector3 (transform.rotation.eulerAngles.x, rotationTarget.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+		transform.rotation = q;
+	}
+	
+	void Update() {
+		transform.position = movementTarget.position + offset;
+		
+		Quaternion q = new Quaternion();
+		q.eulerAngles = new Vector3 (transform.rotation.eulerAngles.x, rotationTarget.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
+		transform.rotation = q;
+	}
+}
